d298e9a09b2a13c3fc5bc38b2d91d0f1c0885c08
Simon Verraest <simon@frak.tl>
2013-09-20 22:02:00 +0200

    Routing

--- web/app.php
+++ web/app.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ApacheRequest;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
@@ -20,6 +21,6 @@ $kernel->loadClassCache();
 $kernel = new AppCache($kernel);
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
-$response = $kernel->handle($request);
+$response = $kernel->handle(ApacheRequest::createFromGlobals());
 $response->send();
 $kernel->terminate($request, $response);
