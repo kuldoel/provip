0bab9b0b8db5efb110ebcf3345c94d0f0978c73e
Simon Verraest <simon@frak.tl>
2013-11-21 15:36:43 +0100

    Document Mimetype Fix

--- src/Provip/ProvipBundle/Entity/Document.php
+++ src/Provip/ProvipBundle/Entity/Document.php
@@ -65,7 +65,7 @@ class Document
 
     /**
      * @Assert\NotBlank
-     * @Assert\File(maxSize="5M", mimeTypes={"application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.ms-powerpoint", "application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/pdf", "application/x-pdf"})
+     * @Assert\File(maxSize="5M", mimeTypes={"application/zip", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.ms-powerpoint", "application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/pdf", "application/x-pdf"})
      *
      * @var UploadedFile
      */
